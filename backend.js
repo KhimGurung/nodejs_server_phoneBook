ba39160bd13b15321dadc04b0cdcb00c15249b6a
KhimGurung <gurungkhim100@gmail.com>
2018-09-24 22:39:38 +0545

    mysql connection pool added

--- backend.js
+++ backend.js
@@ -8,6 +8,7 @@ const database = require("./backend/model/crud");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- backend/model/crud.js
+++ backend/model/crud.js
@@ -1,21 +1,15 @@
 var express = require('express');
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: "root",
-  // password: "root",
-  database: "phonebook",
-  port: 3306,
-  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+var con = mysql.createPool({
+  connectionLimit: 1000,
+  host: "phonebook--mysql.mysql.database.azure.com", 
+  user: "Farzaneh@phonebook--mysql", 
+  password: "adminadmin#23", 
+  database: "phonebook", 
+  port: 3306
 });
 
-con.connect(function(err) {
-  if(err){
-    throw err;
-  }
-  console.log("Connected!");
-});
 
 exports.getAllData = function(callback){
     getRegion(function(region){
@@ -26,22 +20,36 @@ exports.getAllData = function(callback){
         var query = "SELECT * FROM contacts WHERE ContactRegion IN ('"+regions.join("','")+"') ORDER BY ContactName ASC ";
       }
       console.log(query);
-      con.query(query, function (err, result) {
-        if (err) {
-            throw err;
-        } else {
-            callback(err, result);
+      con.getConnection(function(error, tempCon){
+        if(!!error){
+          tempCon.release();
+          console.log('Error');
+        }else{
+          tempCon.query(query, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                callback(err, result);
+            }
+          });
         }
       });
     });
 }
 getRegion = function (callback){
-  con.query("SELECT region FROM selected_region WHERE id=1", function (err, result) {
-    if (err) {
-        throw err;
-    } else {
-        var region = result[0].region;
-        callback(region);
+  con.getConnection(function(error, tempCon){
+    if(!!error){
+      tempCon.release();
+      console.log('Error');
+    }else{
+      tempCon.query("SELECT region FROM selected_region WHERE id=1", function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            var region = result[0].region;
+            callback(region);
+        }
+      });
     }
   });
 }
@@ -55,11 +63,19 @@ exports.searchUser = function(name, callback){
     }else{
       var query = "SELECT * FROM contacts WHERE ContactRegion IN ('"+regions.join("','")+"') AND ContactName LIKE '%"+name+"%' ORDER BY ContactName ASC ";
     }
-    con.query(query, function (err, result) {
-      if (err) {
-          throw err;
-      } else {
-          callback(err, result);
+
+    con.getConnection(function(error, tempCon){
+      if(!!error){
+        tempCon.release();
+        console.log('Error');
+      }else{
+        tempCon.query(query, function (err, result) {
+          if (err) {
+              throw err;
+          } else {
+              callback(err, result);
+          }
+        });
       }
     });
   });
@@ -73,42 +89,71 @@ exports.getImportantContacts = function(callback){
     }else{
       var query = "SELECT * FROM contacts WHERE ContactRegion IN ('"+regions.join("','")+"') AND ContactImportant = 1 ORDER BY ContactName ASC ";
     }
-
-    con.query(query, function (err, result) {
-      if (err) {
-          throw err;
-      } else {
-          callback(err, result);
+    con.getConnection(function(error, tempCon){
+      if(!!error){
+        tempCon.release();
+        console.log('Error');
+      }else{
+        tempCon.query(query, function (err, result) {
+          if (err) {
+              throw err;
+          } else {
+              callback(err, result);
+          }
+        });
       }
     });
   });
 }
 exports.addToImportant = function(id, callback){
-  con.query("UPDATE contacts SET ContactImportant = 1 WHERE ContactID = "+id, function(err, result){
-    if(err){
-      throw err;
+  var query = "UPDATE contacts SET ContactImportant = 1 WHERE ContactID = "+id;
+  con.getConnection(function(error, tempCon){
+    if(!!error){
+      tempCon.release();
+      console.log('Error');
     }else{
-      callback(err, result);
+      tempCon.query(query, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            callback(err, result);
+        }
+      });
     }
   });
 }
 
 exports.removeFromImportant = function(id, callback){
-  con.query("UPDATE contacts SET ContactImportant = 0 WHERE ContactID = "+id, function(err, result){
-    if(err){
-      throw err;
+  var query = "UPDATE contacts SET ContactImportant = 0 WHERE ContactID = "+id;
+  con.getConnection(function(error, tempCon){
+    if(!!error){
+      tempCon.release();
+      console.log('Error');
     }else{
-      callback(result);
+      tempCon.query(query, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            callback(result);
+        }
+      });
     }
   });
 }
 exports.saveSetting = function(region, callback){
-  console.log(region);
-  con.query("UPDATE selected_region SET region = '"+region+"' WHERE id = 1", function(err, result){
-    if(err){
-      throw err;
+  var query = "UPDATE selected_region SET region = '"+region+"' WHERE id = 1";
+  con.getConnection(function(error, tempCon){
+    if(!!error){
+      tempCon.release();
+      console.log('Error');
     }else{
-      callback(result);
+      tempCon.query(query, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            callback(result);
+        }
+      });
     }
   });
 }
